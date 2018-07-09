fcafbff726f7100ce6feb124320b5943605b76b2
Jordan Schuetz <schuetz@pubnub.com>
2018-07-09 11:00:31 -0700

    Add files via upload

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,3 @@
 var AUTH0_CLIENT_ID='a9e2Co7dKmaMhhUVRrQe5TXmtOcObb53';
 var AUTH0_DOMAIN='jordanschuetz.auth0.com';
-var AUTH0_CALLBACK_URL=window.location.href;
+var AUTH0_CALLBACK_URL=window.location.href.substring(0,location.href.lastIndexOf('/')+1)+'vote.html';
