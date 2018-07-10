9d6365affa75c9950326adf0e81bf7ad53c6d311
Jordan Schuetz <schuetz@pubnub.com>
2018-07-10 14:29:22 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -30,28 +30,28 @@ window.addEventListener('load', function () {
     var expiresAt = JSON.stringify(
       authResult.expiresIn * 1000 + new Date().getTime()
     );
-    window.localStorage.setItem('access_token', authResult.accessToken);
-    window.localStorage.setItem('id_token', authResult.idToken);
-    window.localStorage.setItem('expires_at', expiresAt);
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
   }
 
   function logout() {
-    // Remove tokens and expiry time from window.localStorage
-    window.localStorage.removeItem('access_token');
-    window.localStorage.removeItem('id_token');
-    window.localStorage.removeItem('expires_at');
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
   }
 
   function isAuthenticated() {
     // Check whether the current time is past the
     // access token's expiry time
-    var expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
+    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
   }
 
   function getProfile() {
     if (!userProfile) {
-      var accessToken = window.localStorage.getItem('access_token');
+      var accessToken = localStorage.getItem('access_token');
 
       if (!accessToken) {
         console.log('Access token must exist to fetch profile');
